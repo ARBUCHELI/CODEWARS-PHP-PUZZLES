d34b205dc5c6b5b83b8a3aabcfda8a2a626d95d9
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-11-06 10:32:07 +0300

    Create kata1.php

---
+++ kata1.php
@@ -0,0 +1,16 @@
+//This code does not execute properly. Try to figure out why.
+function multiply($a, $b) {
+  return a * b;
+}
+
+//Test
+
+// PHPUnit Test Examples:
+// TODO: Replace examples and use TDD development by writing your own tests
+class MyTestCases extends TestCase
+{
+    public function testThatSomethingShouldHappen() {
+      $this->assertEquals("a", "a");
+      $this->assertEquals([0], [0]);
+    }
+}
